7f19233fe269c10ab8a9450966f0d3354741bd2a
inhye <139088313+ininpark@users.noreply.github.com>
2024-01-19 13:49:27 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,20 @@
+INPUT = input()
+LIST= []
+
+S = {'a':-1,'b':-1,'c':-1,'d':-1,'e':-1,'f':-1,'g':-1,'h':-1,'i':-1,
+'j':-1,'k':-1,'l':-1,'m':-1,'n':-1,'o':-1,'p':-1,'q':-1,'r':-1,
+'s':-1,'t':-1,'u':-1,'v':-1,'w':-1,'x':-1,'y':-1,'z':-1,}
+
+for i in INPUT:
+     LIST.append(i)
+
+cnt = 0 
+for j in INPUT:
+    if S[j] == -1:
+        S[j] = cnt
+        cnt += 1
+    else:
+        cnt += 1
+
+for s in S.values():
+    print(s,end=' ')
