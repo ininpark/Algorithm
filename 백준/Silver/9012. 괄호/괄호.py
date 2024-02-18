657852b4ea7611a708b609650dbe2457c6c6af6d
inhye <139088313+ininpark@users.noreply.github.com>
2024-02-18 18:02:15 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,31 +1,18 @@
-class Stack:
-    def __init__(self):
-        self.s_list =[]
-    def push(self,data):
-        self.s_list.append(data)
-    def pop(self):
-        if not self.isEmpty():
-            self.s_list.pop()
-    def isEmpty(self):
-        return len(self.s_list) == 0
-    def size(self):
-        return len(self.s_list)
-
 T = int(input())
 
 for i in range(T):
     INPUT = list(input())
-    stack = Stack()
+    stack =[]
     NO = 0 
     for j in INPUT:
         if j == '(':
-            stack.push(j)
+            stack.append(j)
         else:
-            if not stack.isEmpty():
+            if len(stack) != 0:
                 stack.pop()
             else:
                 NO += 1
-    if NO == 0 and stack.isEmpty():
+    if NO == 0 and len(stack) == 0:
         print('YES')
     else:
         print('NO')
